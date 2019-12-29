7b760035e9e8bc5bf1fcd827362cff92aa1feef3
Matthew Grohman <mgrohman13@gmail.com>
2019-12-28 18:55:55 -0700

    additional patches

---
+++ QCreatureConfig/Patches/WarpBall_Warp_Patch.cs
@@ -0,0 +1,23 @@
+﻿using Harmony;
+using UnityEngine;
+using System.Reflection;
+
+namespace QCreatureConfig.Patches
+{
+    [HarmonyPatch(typeof(WarpBall))]
+    [HarmonyPatch("Warp")]
+    class WarpBall_Warp_Patch
+    {
+        public static void Prefix(WarpBall __instance)
+        {
+            FieldInfo field = typeof(WarpBall).GetField("warperGO", BindingFlags.Instance | BindingFlags.NonPublic);
+            GameObject warperGO = (GameObject)field.GetValue(__instance);
+            QCreatures.lastTouched = warperGO.GetComponent<Creature>();
+        }
+
+        public static void Postfix()
+        {
+            QCreatures.lastTouched = null;
+        }
+    }
+}
